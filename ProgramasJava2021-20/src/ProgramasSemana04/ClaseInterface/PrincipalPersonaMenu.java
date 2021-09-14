ceb08ae3d14068f4b061c1b4e495a65ab4a8339d
jhorsax <jhorsax161616@gmail.com>
2021-09-14 07:50:35 -0500

    Finalizando semana 4

--- ProgramasJava2021-20/src/ProgramasSemana04/ClaseInterface/PrincipalPersonaMenu.java
+++ ProgramasJava2021-20/src/ProgramasSemana04/ClaseInterface/PrincipalPersonaMenu.java
@@ -1,7 +1,5 @@
 package ProgramasSemana04.ClaseInterface;
 
-import java.io.*;
-
 public class PrincipalPersonaMenu {
 
     public static void main(String[] args) {
@@ -22,7 +20,55 @@ public class PrincipalPersonaMenu {
                 if (op <1 || op >3)
                     System.out.println("Error, Ingrese de nuevo");
             }while (op < 1 || op > 3);
-        }while(5 > 5); //CORREGIR
+            
+            switch(op)
+            {
+                case 1:{
+                    System.out.println("-----Ingrese datos del Empleado------");
+                    
+                    System.out.println("Ingrese Codigo del Empleado: ");
+                    codemp = Integer.parseInt(leerCadena.lectura());
+                    
+                    System.out.println("Ingrese nombre del empleado: ");
+                    nombemp = leerCadena.lectura();
+                    
+                    System.out.println("Ingrese Sueldo del Empleado: ");
+                    sld = Integer.parseInt(leerCadena.lectura());
+                    
+                    Empleado emp = new Empleado(codemp, nombemp, sld);
+                    
+                    System.out.println("==Mostrar Datos del Empleado==");
+                    System.out.println(emp.mostrarDatos());
+                }break;
+                
+                case 2:{
+                    System.out.println("-----Ingrese datos del Docente------");
+                    
+                    System.out.println("Ingrese Codigo del Docente: ");
+                    coddoc = Integer.parseInt(leerCadena.lectura());
+                    
+                    System.out.println("Ingrese nombre del Docente: ");
+                    nombdoc = leerCadena.lectura();
+                    
+                    Docente docen = new Docente(coddoc, nombdoc);
+                    
+                    System.out.println("Ingrese Horas: ");
+                    horas = Integer.parseInt(leerCadena.lectura());
+                    
+                    System.out.println("Ingrese Costo: ");
+                    costo = Integer.parseInt(leerCadena.lectura());
+                    
+                    docen.asignaCarga(horas, costo);
+                    
+                    System.out.println("==Mostrar Datos del Docente==");
+                    System.out.println(docen.mostrarDatos());
+                }break;
+                
+                case 3:{
+                    System.out.println("\tGracias...!!! Vuelva pronto.");
+                }break;
+            }
+        }while(op!=3);
     }
     
 }
